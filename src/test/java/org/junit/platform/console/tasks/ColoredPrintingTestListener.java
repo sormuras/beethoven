954c93460406bd85c27782af35b3daea5133326d
Christian Stein <sormuras@gmail.com>
2016-09-12 23:17:51 +0200

    ASCII art.

--- src/test/java/org/junit/platform/console/tasks/ColoredPrintingTestListener.java
+++ src/test/java/org/junit/platform/console/tasks/ColoredPrintingTestListener.java
@@ -10,18 +10,10 @@
 
 package org.junit.platform.console.tasks;
 
-import static org.junit.platform.console.tasks.ColoredPrintingTestListener.Color.BLUE;
-import static org.junit.platform.console.tasks.ColoredPrintingTestListener.Color.GREEN;
-import static org.junit.platform.console.tasks.ColoredPrintingTestListener.Color.NONE;
-import static org.junit.platform.console.tasks.ColoredPrintingTestListener.Color.PURPLE;
-import static org.junit.platform.console.tasks.ColoredPrintingTestListener.Color.RED;
-import static org.junit.platform.console.tasks.ColoredPrintingTestListener.Color.YELLOW;
-
 import java.io.PrintWriter;
-import java.util.regex.Pattern;
-import org.junit.platform.commons.util.ExceptionUtils;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import org.junit.platform.engine.TestExecutionResult;
-import org.junit.platform.engine.TestExecutionResult.Status;
 import org.junit.platform.engine.reporting.ReportEntry;
 import org.junit.platform.launcher.TestExecutionListener;
 import org.junit.platform.launcher.TestIdentifier;
@@ -30,159 +22,88 @@ import org.junit.platform.launcher.TestPlan;
 /** @since 1.0 */
 class ColoredPrintingTestListener implements TestExecutionListener {
 
-  private static final Pattern LINE_START_PATTERN = Pattern.compile("(?m)^");
-
-  static final String INDENTATION = "             ";
-
   private final PrintWriter out;
-  private final boolean disableAnsiColors;
+  private Deque<TestIdentifier> testIdentifiers = new ArrayDeque<>();
 
   private int treeLevel = 1;
 
   ColoredPrintingTestListener(PrintWriter out, boolean disableAnsiColors) {
     this.out = out;
-    this.disableAnsiColors = disableAnsiColors;
   }
 
   @Override
   public void testPlanExecutionStarted(TestPlan testPlan) {
     out.printf(
-        "Test execution started. Number of static tests: %d%n",
+        "Test plan execution started. Number of static tests: %d%n",
         testPlan.countTestIdentifiers(TestIdentifier::isTest));
   }
 
   @Override
   public void testPlanExecutionFinished(TestPlan testPlan) {
-    out.println("Test execution finished.");
+    out.println("Test plan execution finished.");
   }
 
   @Override
   public void dynamicTestRegistered(TestIdentifier testIdentifier) {
-    printlnTestDescriptor(BLUE, "|", testIdentifier);
+    // ignored in tree printing
   }
 
   @Override
   public void executionSkipped(TestIdentifier testIdentifier, String reason) {
-    printlnTestDescriptor(YELLOW, "Skipped:", testIdentifier);
-    printlnMessage(YELLOW, "Reason", reason);
+    testIdentifiers.push(testIdentifier);
+    out.printf(
+        "%s %s skipped due to: %s %n", indent("-X-"), testIdentifier.getDisplayName(), reason);
+    testIdentifiers.pop();
+    if (testIdentifiers.size() == 1) {
+      out.println(" | ");
+    }
   }
+
+  boolean inline = false;
 
   @Override
   public void executionStarted(TestIdentifier testIdentifier) {
-    printlnTestDescriptor(NONE, "|", testIdentifier);
-    treeLevel++;
+    if (inline) {
+      out.printf("%n");
+      inline = false;
+    }
+    testIdentifiers.push(testIdentifier);
+    String pointer = testIdentifier.isContainer() ? " /-" : "---";
+    out.printf("%s %s", indent(pointer), testIdentifier.getDisplayName());
+    inline = true;
   }
 
   @Override
   public void executionFinished(
       TestIdentifier testIdentifier, TestExecutionResult testExecutionResult) {
-    treeLevel--;
-    Color color = determineColor(testExecutionResult.getStatus());
-    printlnTestDescriptor(color, "\\", testIdentifier);
-    testExecutionResult.getThrowable().ifPresent(t -> printlnException(color, t));
-    if (treeLevel == 2) {
-      out.println("|");
+    if (inline) {
+      out.printf("%n");
+    } else if (testIdentifier.isContainer()) {
+      out.printf("%s %s%n", indent(" \\-"), testIdentifier.getDisplayName());
     }
+    TestIdentifier popped = testIdentifiers.pop();
+    testExecutionResult.getThrowable().ifPresent(t -> out.printf("Exception: %s%n", t));
+    if (testIdentifiers.size() == 1) {
+      out.println(" | ");
+    }
+    inline = false;
   }
 
   @Override
   public void reportingEntryPublished(TestIdentifier testIdentifier, ReportEntry entry) {
-    printlnTestDescriptor(PURPLE, "Reported:", testIdentifier);
-    printlnMessage(PURPLE, "Reported values", entry.toString());
-  }
-
-  private Color determineColor(Status status) {
-    switch (status) {
-      case SUCCESSFUL:
-        return GREEN;
-      case ABORTED:
-        return YELLOW;
-      case FAILED:
-        return RED;
-      default:
-        return NONE;
+    if (inline) {
+      inline = false;
+      out.printf(" reports%n");
     }
+    out.printf("%s %s%n", indent("   "), entry.toString());
   }
 
-  private void printlnTestDescriptor(Color color, String message, TestIdentifier testIdentifier) {
-    println(
-        color,
-        "%s %s (%s)",
-        indent(treeLevel, message),
-        testIdentifier.getDisplayName(),
-        testIdentifier.getUniqueId());
-  }
-
-  private void printlnException(Color color, Throwable throwable) {
-    printlnMessage(color, "Exception", ExceptionUtils.readStackTrace(throwable));
-  }
-
-  private void printlnMessage(Color color, String message, String detail) {
-    println(color, INDENTATION + "=> " + message + ": %s", indented(detail));
-  }
-
-  private void println(Color color, String format, Object... args) {
-    println(color, String.format(format, args));
-  }
-
-  private void println(Color color, String message) {
-    if (disableAnsiColors) {
-      out.println(message);
-    } else {
-      // Use string concatenation to avoid ANSI disruption on console
-      out.println(color + message + NONE);
-    }
-  }
-
-  private static String indent(int level, String pointer) {
+  private String indent(String pointer) {
     StringBuilder builder = new StringBuilder();
-    for (int i = 0; i < level - 1; i++) {
-      builder.append("| ");
+    for (int i = 0; i < testIdentifiers.size() - 1; i++) {
+      builder.append(" | ");
     }
     builder.append(pointer);
     return builder.toString();
-  }
-
-  /**
-   * Indent the given message if it is a multi-line string.
-   *
-   * <p>{@link #INDENTATION} is used to prefix the start of each new line except the first one.
-   *
-   * @param message the message to indent
-   * @return indented message
-   */
-  private static String indented(String message) {
-    return LINE_START_PATTERN.matcher(message).replaceAll(INDENTATION).trim();
-  }
-
-  enum Color {
-    NONE(0),
-
-    BLACK(30),
-
-    RED(31),
-
-    GREEN(32),
-
-    YELLOW(33),
-
-    BLUE(34),
-
-    PURPLE(35),
-
-    CYAN(36),
-
-    WHITE(37);
-
-    private final int ansiCode;
-
-    Color(int ansiCode) {
-      this.ansiCode = ansiCode;
-    }
-
-    @Override
-    public String toString() {
-      return "\u001B[" + this.ansiCode + "m";
-    }
   }
 }
