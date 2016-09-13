7177a75a710595e14cdbdbeb4b3164b5e941bb5c
Christian Stein <sormuras@gmail.com>
2016-09-13 11:47:54 +0200

    Introduced tile set enum.

--- src/test/java/org/junit/platform/console/tasks/ColoredPrintingTestListener.java
+++ src/test/java/org/junit/platform/console/tasks/ColoredPrintingTestListener.java
@@ -13,6 +13,7 @@ package org.junit.platform.console.tasks;
 import java.io.PrintWriter;
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.Optional;
 import org.junit.platform.engine.TestExecutionResult;
 import org.junit.platform.engine.reporting.ReportEntry;
 import org.junit.platform.launcher.TestExecutionListener;
@@ -22,8 +23,30 @@ import org.junit.platform.launcher.TestPlan;
 /** @since 1.0 */
 class ColoredPrintingTestListener implements TestExecutionListener {
 
+  enum Tile {
+    CONTAINER_BEGIN(" +--", "┌─"),
+
+    CONTAINER_END(" +--", "└─"),
+
+    REPORT("r"),
+
+    SKIP("x"),
+
+    TEST("o", "»"),
+
+    VERTICAL(" | ", "│");
+
+    String[] set;
+
+    Tile(String... set) {
+      this.set = set;
+    }
+  }
+
+  private Deque<TestIdentifier> deque = new ArrayDeque<>();
+  private Optional<TestIdentifier> justFinished = Optional.empty();
+  private int tileSet = 0;
   private final PrintWriter out;
-  private Deque<TestIdentifier> testIdentifiers = new ArrayDeque<>();
 
   ColoredPrintingTestListener(PrintWriter out, boolean disableAnsiColors) {
     this.out = out;
@@ -42,52 +65,45 @@ class ColoredPrintingTestListener implements TestExecutionListener {
   }
 
   @Override
-  public void dynamicTestRegistered(TestIdentifier testIdentifier) {
+  public void dynamicTestRegistered(TestIdentifier id) {
     // ignored in tree printing
   }
 
   @Override
-  public void executionSkipped(TestIdentifier testIdentifier, String reason) {
-    testIdentifiers.push(testIdentifier);
-    out.printf(
-        "%s %s skipped due to: %s %n", indent("-X-"), testIdentifier.getDisplayName(), reason);
-    testIdentifiers.pop();
-    if (testIdentifiers.size() == 1) {
-      out.println("|");
-    }
-    out.flush();
+  public void executionSkipped(TestIdentifier id, String reason) {
+    deque.push(id);
+    out.printf("%s %s skipped due: %s%n", indent(Tile.SKIP), id.getDisplayName(), reason);
+    deque.pop();
   }
 
   boolean inline = false;
 
   @Override
-  public void executionStarted(TestIdentifier testIdentifier) {
+  public void executionStarted(TestIdentifier id) {
     if (inline) {
       out.printf("%n");
       inline = false;
     }
-    testIdentifiers.push(testIdentifier);
-    String pointer = testIdentifier.isContainer() ? "┌─" : " »";
-    out.printf("%s %s", indent(pointer), testIdentifier.getDisplayName());
+    if (deque.size() == 1 && justFinished.isPresent()) {
+      out.printf("%s%n", indent(Tile.VERTICAL));
+    }
+    deque.push(id);
+    Tile tile = id.isContainer() ? Tile.CONTAINER_BEGIN : Tile.TEST;
+    out.printf("%s %s", indent(tile), id.getDisplayName());
     inline = true;
-    out.flush();
   }
 
   @Override
-  public void executionFinished(
-      TestIdentifier testIdentifier, TestExecutionResult testExecutionResult) {
+  public void executionFinished(TestIdentifier id, TestExecutionResult testExecutionResult) {
     if (inline) {
       out.printf("%n");
-    } else if (testIdentifier.isContainer()) {
-      out.printf("%s %s%n", indent("└─"), testIdentifier.getDisplayName());
+    } else if (id.isContainer()) {
+      out.printf("%s %s%n", indent(Tile.CONTAINER_END), id.getDisplayName());
+      out.flush();
     }
-    testIdentifiers.pop();
     testExecutionResult.getThrowable().ifPresent(t -> out.printf("Exception: %s%n", t));
-    if (testIdentifiers.size() == 1) {
-      out.println("│");
-    }
+    justFinished = Optional.of(deque.pop());
     inline = false;
-    out.flush();
   }
 
   @Override
@@ -96,16 +112,15 @@ class ColoredPrintingTestListener implements TestExecutionListener {
       inline = false;
       out.printf(" reports%n");
     }
-    out.printf("%s %s%n", indent("   "), entry.toString());
-    out.flush();
+    out.printf("%s %s%n", indent(Tile.REPORT), entry.toString());
   }
 
-  private String indent(String pointer) {
+  private String indent(Tile tile) {
     StringBuilder builder = new StringBuilder();
-    for (int i = 0; i < testIdentifiers.size() - 1; i++) {
-      builder.append("│");
+    for (int i = 0; i < deque.size() - 1; i++) {
+      builder.append(Tile.VERTICAL.set[tileSet]);
     }
-    builder.append(pointer);
+    builder.append(tile.set[tileSet]);
     return builder.toString();
   }
 }
