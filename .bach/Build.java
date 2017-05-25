e01d10b12b6c49214714319bb73a34e96bd6fa44
Christian Stein <sormuras@gmail.com>
2017-05-25 10:01:51 +0200

    Load 3rd-party-modules needed for testing

--- .bach/Build.java
+++ .bach/Build.java
@@ -1,3 +1,4 @@
+import java.net.URI;
 import java.nio.file.Paths;
 import java.util.logging.Level;
 
@@ -9,9 +10,20 @@ public class Build {
         .override(Folder.SOURCE, Paths.get("modules"))
         .override(Folder.DEPENDENCIES, Paths.get("dependencies"))
       .build()
+        // JUnit Jupiter API
+        .load("org.junit.jupiter.api", URI.create("http://central.maven.org/maven2/org/junit/jupiter/junit-jupiter-api/5.0.0-M4/junit-jupiter-api-5.0.0-M4.jar"))
+        .load("org.junit.platform.commons", URI.create("http://central.maven.org/maven2/org/junit/platform/junit-platform-commons/1.0.0-M4/junit-platform-commons-1.0.0-M4.jar"))
+        .load("org.opentest4j", URI.create("http://central.maven.org/maven2/org/opentest4j/opentest4j/1.0.0-M2/opentest4j-1.0.0-M2.jar"))
+        // JQwik Test Engine
+        .load("com.github.jlink.jqwik", URI.create("https://jitpack.io/com/github/jlink/jqwik/0.3.0/jqwik-0.3.0.jar"))
+        .load("org.junit.platform.engine", URI.create("http://central.maven.org/maven2/org/junit/platform/junit-platform-engine/1.0.0-M4/junit-platform-engine-1.0.0-M4.jar"))
+        //
+        //
+        //
         .format()
         .compile()
-        .run("de.sormuras.beethoven", "de.sormuras.beethoven.Beethoven");
+        .run("de.sormuras.beethoven", "de.sormuras.beethoven.Beethoven")
+        .test();
   }
 
 }
